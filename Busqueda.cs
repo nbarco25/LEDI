c9d82337f0d980e586a7a71ca82aa76a9a11bb34
Your Name <you@example.com>
2016-09-01 04:10:54 -0500

    Primera modificacion

---
+++ Busqueda.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ledi_3
+{
+    class Busqueda : Array
+    {
+        /*busqueda lineal secuencial --> metodo 1; el vector esta desordenado
+          busqueda lineal recursiva --> metodo 2; el vector esta desordenado
+          busqueda binaria secuencial --> metodo 3 el vector DEBE estar ordenado
+          busqueda binaria recursiva -- metodo 4  el vector DEBE estar ordenado
+
+        */
+
+        private int elemento;
+
+        public int Elemento
+        {
+            get
+            {
+                return elemento;
+            }
+
+            set
+            {
+                elemento = value;
+            }
+        }
+        
+
+        public void busquedaLineal(int[] arreglo)
+        {
+            
+
+        }
+    }
+}
