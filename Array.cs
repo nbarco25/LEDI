5da7c42931970ac80eea6bf6cc7ba33b8dcd862f
Your Name <you@example.com>
2016-09-01 10:04:04 -0500

    otras clases

---
+++ Array.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ledi_3
+{
+    class Array
+    {
+        //incializa, ordena, y muestra el vector
+        #region atributos
+        private int[] numeros;
+        private int numElementos;
+        private int min;
+        private int max;
+        private int tamaño;
+        #endregion
+        
+        #region metodos
+
+        #region setters & getters
+       
+
+        public int NumElementos
+        {
+            get
+            {
+                return numElementos;
+            }
+
+            set
+            {
+                numElementos = value;
+            }
+        }
+
+        public int Min
+        {
+            get
+            {
+                return min;
+            }
+
+            set
+            {
+                min = value;
+            }
+        }
+
+        public int Max
+        {
+            get
+            {
+                return max;
+            }
+
+            set
+            {
+                max = value;
+            }
+        }
+
+        public int Tamaño
+        {
+            get
+            {
+                return tamaño;
+            }
+
+            set
+            {
+                tamaño = value;
+            }
+        }
+
+        public int[] Numeros
+        {
+            get
+            {
+                return numeros;
+            }
+
+            set
+            {
+                numeros = value;
+            }
+        }
+
+        #endregion
+
+        #region metodosPropios
+
+        public void generarAleatorios()
+        {
+            Random aleatorios = new Random();
+            Numeros = new int[Tamaño];
+            for (int i = 0; i < Tamaño; i++)
+            {
+                Numeros[i] = aleatorios.Next(Min, Max);
+            }
+        }
+        public void ordenarVector()
+        {
+            for (int i = 0; i < Tamaño - 1; i++)
+            {
+                for (int j = i + 1; j < Tamaño; j++)
+                {
+                    if (Numeros[i] > Numeros[j])
+                    {
+                        int aux = Numeros[i];
+                        Numeros[i] = Numeros[j];
+                        Numeros[j] = aux;
+                    }
+                }
+            }
+        }
+        public void imprimirVector()
+        {
+            for (int i = 0; i < Tamaño; i++)
+            {
+                Console.Write(Numeros[i]+"|");
+            }
+        }
+        #endregion
+
+        #endregion
+
+        /*Metodo generar numeros aleatorios que inicialice el vector*/
+    }
+}
---
+++ Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ledi_3
+{
+    class Test
+    {
+        static void Main(string[] args)
+        {
+            #region instancias
+            Array v1 = new Array();
+            Busqueda b = new Busqueda();
+            #endregion
+
+            Console.Write("Ingrese el valor minimo del vector a generar");
+            v1.Min = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el valor maximo del vecror a generar");
+            v1.Max = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Ingrese el tamaño del vector");
+            v1.Tamaño = Convert.ToInt32(Console.ReadLine());
+            v1.generarAleatorios();
+            v1.imprimirVector();
+            v1.ordenarVector();
+            v1.imprimirVector();
+
+            Console.ReadKey();
+        }
+    }
+}
